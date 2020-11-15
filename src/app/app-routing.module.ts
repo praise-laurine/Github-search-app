bad2a6b2bdcc0c953834310de552cf8e60039bb1
praise-laurine <praiselaurine10@gmail.com>
2020-11-16 01:51:25 +0300

    added router-outlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RepositoryComponent } from "../app/repository/repository.component";
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'repository', component: RepositoryComponent},
+  { path: '', redirectTo:"/repository", pathMatch:"full"},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
